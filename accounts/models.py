73784f521e1c430d7dc0cc91005b4f12cc054098
Murad <merch.fade13@gmail.com>
2020-07-24 18:04:54 +0300

    max_length of phones is 25 p2

--- accounts/models.py
+++ accounts/models.py
@@ -83,7 +83,7 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     phone_regex = RegexValidator(regex=r'^\+?1?\d{11,25}$',
                                 message="Phone number must be entered in the format: '+999999999'. Up to 14 digits allowed.")
-    phone       = models.CharField(validators = [phone_regex], max_length=20, unique=True)
+    phone       = models.CharField(validators = [phone_regex], max_length=25, unique=True)
     name        = models.CharField(max_length=20, blank=True, null=True)
     first_login = models.BooleanField(default=False)
     active      = models.BooleanField(default=True)
@@ -133,7 +133,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 class PhoneOTP(models.Model):
     phone_regex = RegexValidator(regex=r'^\+?1?\d{11,25}$',
                                 message="Phone number must be entered in the format: '+999999999'. Up to 14 digits allowed.")
-    phone       = models.CharField(validators = [phone_regex], max_length=20, unique=True)
+    phone       = models.CharField(validators = [phone_regex], max_length=25, unique=True)
     otp         = models.CharField(max_length = 9, blank = True, null = True)
     count       = models.IntegerField(default = 0, help_text = 'Number of otp sent')
     validated   = models.BooleanField(default = False, help_text='If it is true, that means user have validate otp correctly in second API')
